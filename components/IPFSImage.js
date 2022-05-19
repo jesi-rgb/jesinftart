37b93228ac9f071d03904e48d4ba24e3ba60d8b2
Jesús Enrique Rascón <blograso@gmail.com>
2022-05-19 10:42:32 +0200

    created ipfs image component

--- components/IPFSImage.js
+++ components/IPFSImage.js
@@ -1,15 +1,30 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 export default function IPFSImage({ hash }) {
   return (
     <>
-      <div>
-        <Image
-          src={"https://ipfs.io/ipfs/" + hash}
-          alt=""
-          width={500}
-          height={500}
-        />
+      <div className="w-max flex-shrink-0">
+        <div className="flex-col ">
+          <a href={`https://${hash}.ipfs.infura-ipfs.io/`}>
+            <img
+              src={`https://${hash}.ipfs.infura-ipfs.io/`}
+              alt=""
+              width={500}
+              height={500}
+            />
+          </a>
+          <p className="w-48 text-gray-400 font-body truncate ...">
+            {shrinkHash(hash)}
+          </p>
+        </div>
       </div>
     </>
   );
+}
+
+function shrinkHash(hash) {
+  let end = hash.slice(hash.length - 4);
+  let start = hash.slice(0, 4);
+  console.log(end + "..." + start);
+  return start + "..." + end;
 }
--- pages/index.js
+++ pages/index.js
@@ -8,37 +8,96 @@ export default function Home() {
     <Layout>
       <section className="text-gray-300 font-body">
         <div className="container px-5 py-24 mx-auto">
-          <h1 className="text-6xl font-bold font-titles text-center text-gray-100 mb-20">
+          <h1 className="text-6xl drop-shadow-xl font-bold font-titles text-center text-gray-100 mb-20">
             Jesi NFT Market Place
           </h1>
 
-          <div className="flex flex-col space-y-6 lg:flex-row lg:space-x-6">
-            <IPFSImage
-              hash={
-                "bafybeiarr6v3ajdv5iwiwmehmcn4tqxpdpnenuokrgvon5odg6ujev2xvi"
-              }
-            />
-
+          <div className="flex flex-col mx-auto w-full rounded-md drop-shadow-2xl lg:flex-row overflow-x-scroll scrollbar-hide space-x-10">
             <IPFSImage
               hash={
                 "bafybeidwzynbvhtc6qspgkn7amkgd4nacubpxoezul4qroiqdf2vympr3u"
               }
             />
-          </div>
-          <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
-            <div className="p-4 md:mx-auto md:max-w-sm flex">
-              <div className="flex-grow pl-6">
-                <h2 className="text-gray-100 text-lg font-bold mb-2">
-                  Cock and Bolardas gimme my banana
-                </h2>
-                <p className="leading-relaxed text-base">
-                  Hola alex esto es una prueba que dices te mola?
-                </p>
-                <a className="mt-3 text-gray-600 inline-flex items-center">
-                  Learn More
-                </a>
-              </div>
-            </div>
+            <IPFSImage
+              hash={
+                "bafybeidwzynbvhtc6qspgkn7amkgd4nacubpxoezul4qroiqdf2vympr3u"
+              }
+            />
+            <IPFSImage
+              hash={
+                "bafybeidwzynbvhtc6qspgkn7amkgd4nacubpxoezul4qroiqdf2vympr3u"
+              }
+            />
+            <IPFSImage
+              hash={
+                "bafybeidwzynbvhtc6qspgkn7amkgd4nacubpxoezul4qroiqdf2vympr3u"
+              }
+            />
+            <IPFSImage
+              hash={
+                "bafybeidwzynbvhtc6qspgkn7amkgd4nacubpxoezul4qroiqdf2vympr3u"
+              }
+            />
+            <IPFSImage
+              hash={
+                "bafybeidwzynbvhtc6qspgkn7amkgd4nacubpxoezul4qroiqdf2vympr3u"
+              }
+            />
+            <IPFSImage
+              hash={
+                "bafybeiarr6v3ajdv5iwiwmehmcn4tqxpdpnenuokrgvon5odg6ujev2xvi"
+              }
+            />
+            <IPFSImage
+              hash={
+                "bafybeidwzynbvhtc6qspgkn7amkgd4nacubpxoezul4qroiqdf2vympr3u"
+              }
+            />
+            <IPFSImage
+              hash={
+                "bafybeiarr6v3ajdv5iwiwmehmcn4tqxpdpnenuokrgvon5odg6ujev2xvi"
+              }
+            />
+            <IPFSImage
+              hash={
+                "bafybeidwzynbvhtc6qspgkn7amkgd4nacubpxoezul4qroiqdf2vympr3u"
+              }
+            />
+            <IPFSImage
+              hash={
+                "bafybeiaobfbg56q6vcmcs7o3qya4enftunly36akfzei4oyznwv4633o54"
+              }
+            />
+            <IPFSImage
+              hash={
+                "bafybeiaobfbg56q6vcmcs7o3qya4enftunly36akfzei4oyznwv4633o54"
+              }
+            />
+            <IPFSImage
+              hash={
+                "bafybeiaobfbg56q6vcmcs7o3qya4enftunly36akfzei4oyznwv4633o54"
+              }
+            />
+            <IPFSImage
+              hash={
+                "bafybeiaobfbg56q6vcmcs7o3qya4enftunly36akfzei4oyznwv4633o54"
+              }
+            />
+            <IPFSImage
+              hash={
+                "bafybeiaobfbg56q6vcmcs7o3qya4enftunly36akfzei4oyznwv4633o54"
+              }
+            />
+            <IPFSImage
+              hash={
+                "bafybeiaobfbg56q6vcmcs7o3qya4enftunly36akfzei4oyznwv4633o54"
+              }
+            />
+            <IPFSImage
+              hash={
+                "bafybeiaobfbg56q6vcmcs7o3qya4enftunly36akfzei4oyznwv4633o54"
+              }
+            />
           </div>
         </div>
       </section>
