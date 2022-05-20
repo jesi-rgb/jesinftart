dda50e500487926a8860fdbd1efcb23524866c75
Jesús Enrique Rascón <blograso@gmail.com>
2022-05-20 12:31:41 +0200

    ole

--- components/PieceBody.js
+++ components/PieceBody.js
@@ -8,17 +8,10 @@ export default function PieceBody(slug) {
         {/* Image and hash */}
         <div className="w-full flex-col mb-10 space-y-6 mt-14 mx-auto lg:max-w-min lg:w-1/2 lg:mb-0">
           <h1 className="text-4xl lg:text-6xl font-titles text-gray-100 mx-auto">
-            Title placeholder
+            Title
           </h1>
           <div className="mx-auto">
             <IPFSImage hash={slug} />
-          </div>
-          <div className="">
-            <div className="text-gray-200 font-titles"> Hash:</div>
-            <div className="text-gray-400 font-body text-xs lg:text-lg w-min">
-              {" "}
-              {slug.content}
-            </div>
           </div>
         </div>
         {/* NFT DATA */}
@@ -43,11 +36,21 @@ export default function PieceBody(slug) {
           </div>
           <div className="flex flex-row font-body">
             <div className="flex text-gray-200 font-titles">Owner</div>
-            <div
-              className="flex text-gray-400 ml-4 truncate ..."
-              alt="0000002304990291348102934801293481029348"
-            >
-              {shrinkHash("0000002304990291348102934801293481029348")}
+
+            <div>
+              <div className="flex text-gray-400 ml-4 truncate ...">
+                {shrinkHash("0000002304990291348102934801293481029348")}
+              </div>
+              <div className="text-gray-400 ml-4 opacity-0 hover:opacity-100 transition-opacity duration-300">
+                0000002304990291348102934801293481029348
+              </div>
+            </div>
+          </div>
+          <div className="">
+            <div className="text-gray-200 font-titles"> Hash:</div>
+            <div className="text-gray-400 font-body text-xs lg:text-lg w-min">
+              {" "}
+              {shrinkHash(slug.content)}
             </div>
           </div>
         </div>
