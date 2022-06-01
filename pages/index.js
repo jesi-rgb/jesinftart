b244f2933747086660e662054d8a168457db1128
Alex de la Cruz <alex@Alexs-MacBook-Air.local>
2022-06-01 10:06:35 +0200

    test

--- pages/index.js
+++ pages/index.js
@@ -75,7 +75,7 @@ export default function Home() {
       <div className="text-slate-300 font-body">
         <div className="mt-12 mx-auto lg:max-w-min sm:max-w-max">
           <h1 className="text-6xl drop-shadow-xl mx-auto font-bold font-titles text-center text-slate-100 mb-10">
-            WebItOS
+            WebOtES
           </h1>
 
           <div className="flex flex-col space-y-16 rounded-md drop-shadow-2xl w-full lg:flex-row lg:overflow-x-scroll lg:scrollbar-hide lg:space-y-0 lg:space-x-10">
