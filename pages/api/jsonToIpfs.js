1d363eb18e9b08df854983b26296bd4f645034f6
Alex de la Cruz <alex@Alexs-Air.home>
2022-08-04 12:06:15 +0200

    added error log2

--- pages/api/jsonToIpfs.js
+++ pages/api/jsonToIpfs.js
@@ -10,17 +10,24 @@ const pinata_client = pinata(
  * Uploads json to IPFS
  */
 export default async (req, res) => {
-  pinata_client.pinJSONToIPFS(req.body).then(
-    (value) => {
-      res.setHeader("Content-Type", "application/json");
-      res.setHeader("Cache-Control", "max-age=180000");
-      res.end(JSON.stringify(value));
-    },
-    (error) => {
-      res.setHeader("Content-Type", "application/json");
-      res.setHeader("Cache-Control", "max-age=180000");
-      res.status(500);
-      res.end(error);
-    }
-  );
+  try {
+    pinata_client.pinJSONToIPFS(req.body).then(
+      (value) => {
+        res.setHeader("Content-Type", "application/json");
+        res.setHeader("Cache-Control", "max-age=180000");
+        res.end(JSON.stringify(value));
+      },
+      (error) => {
+        res.setHeader("Content-Type", "application/json");
+        res.setHeader("Cache-Control", "max-age=180000");
+        res.status(500);
+        res.end(error);
+      }
+    );
+  } catch (error) {
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Cache-Control", "max-age=180000");
+    res.status(500);
+    res.end(error);
+  }
 };
